aa97f452cbb93407aad0397eb50b0eb533c8d089
Brody <113292266+b-geo@users.noreply.github.com>
2025-02-26 17:18:56 +0800

    update to run structure

--- afl_games/run.py
+++ afl_games/run.py
@@ -2,17 +2,17 @@ from pathlib import Path
 import yaml
 from importlib import import_module
 
+#get config for each pipeline
 yaml_file_path = __file__.replace(".py", ".yaml")
 if Path(yaml_file_path).exists():
     with open(yaml_file_path) as yaml_file:
         pipeline_config = yaml.safe_load(yaml_file)
         pipeline_list = [pipeline for pipeline in pipeline_config.get("pipelines")]
-        # config = pipeline_config.get("config")
 else:
     raise Exception(
         f"Missing {yaml_file_path} file! Please create the yaml file with at least a `name` key for the pipeline name."
     )
-print(pipeline_list)
+#run each pipeline
 for pipeline in pipeline_list:
     pipeline_name = pipeline.get("name")
     pipeline_config = pipeline.get("config")
