c26e09431a77cb382e318ab831748dd5129cae8b
Brody <113292266+b-geo@users.noreply.github.com>
2025-02-27 16:32:37 +0800

    integration testing

---
+++ afl_games/test/connectors/test_squiggle_api.py
@@ -0,0 +1,26 @@
+import pytest
+import os
+import datetime
+from dotenv import load_dotenv
+from main.connectors.squiggle_api import SquiggleApiClient
+
+@pytest.fixture(scope="session")
+def squiggle_client():
+    load_dotenv()
+    #squiggle user agent
+    SQUIGGLE_USER_AGENT = os.environ.get("SQUIGGLE_USER_AGENT")
+    return SquiggleApiClient(SQUIGGLE_USER_AGENT)
+
+def test_games_response(squiggle_client):
+    this_year = datetime.date.today().year
+    games_data = squiggle_client.get_games(year = this_year)
+    assert len(games_data) > 0
+    assert isinstance(games_data, dict)
+    assert "games" in games_data
+
+def test_odds_response(squiggle_client):
+    this_year = datetime.date.today().year
+    odds_data = squiggle_client.get_odds(year = this_year, round = 1)
+    assert len(odds_data) > 0
+    assert isinstance(odds_data, dict)
+    assert "tips" in odds_data
