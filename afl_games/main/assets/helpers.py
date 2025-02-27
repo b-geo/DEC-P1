f76f7c1b77e765209fedd364375cf9445a63a7c5
Brody <113292266+b-geo@users.noreply.github.com>
2025-02-27 18:32:28 +0800

    added units tests

--- afl_games/main/assets/helpers.py
+++ afl_games/main/assets/helpers.py
@@ -4,12 +4,13 @@ from sqlalchemy import Table
 #use as test table
 # from load: compare_table_schema(table, postgresql_client.get_table_schema(table_name=table.name))
 def compare_table_schema(source_table: Table, target_table: Table) -> None:
-    """checks that the tables declared in metadata are actually in the db"""
+    """checks if table in db is same as table that is to be loaded"""
     for source_column in source_table.c:
         if source_column.name not in target_table.c:
-            raise Exception(f"Table {target_table.name} in target DB does not contain column {source_column.name}")
+            raise Exception(f"Table {target_table.name} in target DB does not contain column {source_column.name}.")
         target_column = target_table.c[source_column.name]
         if not isinstance(target_column.type, source_column.type.__class__):
+            print(f"Source column '{source_column.name}' is {source_column.type} but expected {target_column.type}.")
             raise Exception(f"Source column '{source_column.name}' is {source_column.type} but expected {target_column.type}.")
     #should i return false if not the same then check for that?
 
